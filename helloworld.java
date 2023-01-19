6d5456614f1252e1026f0050a2a4f7ad4432cc41
Kaishav Basodiya <85062137+Kaishav65@users.noreply.github.com>
2023-01-19 11:49:15 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello from Aditya Priyanshu 202051010.");
+    System.out.println("Hello from Kaishav Basodiya 202051094.");
     }
 }
